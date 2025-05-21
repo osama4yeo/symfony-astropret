71562169594d6e7f216de9ed87466286f21f09fe
eddygrdj <graradji.eddy@gmail.com>
2025-05-21 08:49:02 +0200

    Premier dépot

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -20,4 +20,21 @@ class UserController extends AbstractController
             'users' => $users,
         ]);
     }
+
+    #[IsGranted('ROLE_USER')]
+    #[Route('/profil', name: 'user_profile')]
+    public function monProfil(): Response
+    {
+        $user = $this->getUser();
+
+        $response = $this->render('profil.html.twig', [
+            'user' => $user,
+        ]);
+    
+        // Empêche la mise en cache par le navigateur
+        $response->headers->set('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
+        $response->headers->set('Pragma', 'no-cache');
+    
+        return $response;
+    }
 }
